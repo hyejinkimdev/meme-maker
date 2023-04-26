a46d7b2a3148468de2ed04153c8783ce8c481810
Hyejin Kim <hyejinkim.dev@gmail.com>
2023-04-26 12:40:08 +0200

    Add a file button, a text button and saving button

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 // const colorOpthions = document.getElementsByClassName("color-option");
+const saveBtn = document.getElementById("save");
+const textInput = document.getElementById("text");
+const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const eraserBtn = document.getElementById("eraser-btn");
-const colorOpthions = Array.from(document.getElementsByClassName("color-option"));
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-
-ctx.lineWidth = lineWidth.value;
-let isPainsting = false;
-let isFilling = false;
 
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
 
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
+ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
+let isPainsting = false;
+let isFilling = false;
+
+
+
+
 
 function onMove(event){
     if(isPainsting){
@@ -36,7 +43,7 @@ function cancelPainting (){
 }
 
 function onLineWidthChange(event){
-    console.log(event.target.value);
+    // console.log(event.target.value);
     ctx.lineWidth = event.target.value;
 }
 
@@ -79,6 +86,40 @@ function onEraserClick(){
     modeBtn.innerText = "Fill";
 }
 
+function onFileChange(event){
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src  = url;
+    image.onload = function () {
+        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        fileInput.value = null;
+    }
+    // console.dir(event.target);
+    // console.dir(url);
+};
+
+function onDoubleClick(event){
+    
+    const text = textInput.value;
+    if (text !== ""){
+        ctx.save();
+        ctx.lineWidth = 1;
+        ctx.font = "48px serif"
+        ctx.fillText(text, event.offsetX, event.offsetY);
+        ctx.restore();
+    }
+}
+
+function onSaveClick(){
+    const url = canvas.toDataURL();
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "myDrawing.png";
+    a.click();
+}
+
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -88,10 +129,11 @@ canvas.addEventListener("click", onCanvasClick);
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
-console.log(colorOpthions);
+// console.log(colorOptions);
 
-colorOpthions.forEach(color => color.addEventListener("click", onColorClick));
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
 
 modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
 eraserBtn.addEventListener("click", onEraserClick);
+saveBtn.addEventListener("click", onSaveClick);
